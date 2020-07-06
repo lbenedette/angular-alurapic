00a8bb26d6e471e7f325e7fb8e1baaeacf33cd70
Leonardo Lopes <blopesleo@outlook.com>
2020-07-06 12:35:43 -0300

    Unidirectional data-binding in html attributes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'alurapic';
+  url = "https://http.cat/100";
+  description = "100 Error";
 }
