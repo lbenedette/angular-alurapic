c9cc84e6c11879515a2515337ecadf47244a0ee6
Leonardo Lopes <blopesleo@outlook.com>
2020-07-08 12:00:09 -0300

    Comunication between parent and child components, using output properties

--- src/app/photos/photo-list/photo-list.module.ts
+++ src/app/photos/photo-list/photo-list.module.ts
@@ -5,6 +5,7 @@ import { CommonModule } from '@angular/common';
 import { PhotoListComponent } from './photo-list.component';
 import { PhotosComponent } from './photos/photos.component';
 import { LoadButtonComponent } from './load-button/load-button.component';
+import { SearchComponent } from './search/search.component';
 import { FilterByDescriptionPipe } from './filter-by-description.pipe';
 import { PhotoModule } from '../photo/photo.module';
 import { CardModule } from '../../shared/components/card/card.module';
@@ -14,6 +15,7 @@ import { CardModule } from '../../shared/components/card/card.module';
     PhotoListComponent,
     PhotosComponent,
     LoadButtonComponent,
+    SearchComponent,
     FilterByDescriptionPipe
   ],
   imports: [
