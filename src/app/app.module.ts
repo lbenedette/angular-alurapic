db916003637d71555a7c817a02f80ee6c55aaf59
Leonardo Lopes <blopesleo@outlook.com>
2020-07-09 11:35:56 -0300

    Authentication, using BehaviourSubject to deal with pos render after service emit next event

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,11 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { CoreModule } from './core/core.module';
 import { PhotosModule } from './photos/photos.module';
 import { HomeModule } from './home/home.module';
 import { ErrorsModule } from './errors/errors.module';
+import { HeaderComponent } from './core/header/header.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +18,8 @@ import { ErrorsModule } from './errors/errors.module';
     AppRoutingModule,
     PhotosModule,
     HomeModule,
-    ErrorsModule
+    ErrorsModule,
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/core/auth/auth.service.ts
+++ src/app/core/auth/auth.service.ts
@@ -1,14 +1,29 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { tap } from 'rxjs/operators';
+
+import { UserService } from '../user/user.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor(private http: HttpClient) { }
+  constructor(
+    private http: HttpClient,
+    private userService: UserService
+  ) { }
 
   authenticate(username: string, password: string) {
-    return this.http.post('http://localhost:3000/user/login', { userName: username, password });
+    return this.http
+      .post(
+        'http://localhost:3000/user/login',
+        { userName: username, password },
+        { observe: 'response' }
+      )
+      .pipe(tap(response => {
+        const authToken = response.headers.get('x-access-token');
+        this.userService.setToken(authToken);
+      }));
   }
 }
